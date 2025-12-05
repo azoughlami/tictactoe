1e994db53cd8a20eb1e3b2b38e3f4b18300f4e2a
azoughlami <azoughlami@gmail.com>
2025-12-05 17:45:43 +0100

    wip

--- game.py
+++ game.py
@@ -1,17 +1,59 @@
-# create the board
-# choose initial player
-# until someone wins, check the winner
+# v -> choose players
+# v ->create the board
+# v ->choose initial player
+# v -> until someone wins, check the winner
 #   show the board
 #   choose location, mark it
 #   toogle active player
 
 
+
+
+
 def main():
+    #create the board
     board = [
         [None,None,None],
         [None, None, None],
         [None, None, None]
     ]
+
+    #Choose initial player
+    active_player_index = 0
+    players = ["You", "Computer"]
+    symbols = ["X","O"]
+
+    #until someone wins
+    while not find_winner(board):
+        #show board
+        player = players[active_player_index]
+
+        announce_turn(player)
+        show_board(board)
+        input("paused")
+
+
+def show_board(board):
+    for row in board:
+        print("|")
+        for cell in row:
+            print(cell, end = " | ")
+
+        print()
+
+
+
+def announce_turn(player):
+    print()
+    print(f"its {player}'s turn. Here is the board:")
+    print()
+
+
+def find_winner(board):
+    #TODO: implement how we check for the winner
+    return False
+
+
 
 
 
