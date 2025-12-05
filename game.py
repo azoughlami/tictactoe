62cbf9adb1f60cabbf79088d3e88fe96b7e38f79
azoughlami <azoughlami@gmail.com>
2025-12-05 16:29:29 +0100

    board created

--- game.py
+++ game.py
@@ -1,1 +1,19 @@
-check 
+# create the board
+# choose initial player
+# until someone wins, check the winner
+#   show the board
+#   choose location, mark it
+#   toogle active player
+
+
+def main():
+    board = [
+        [None,None,None],
+        [None, None, None],
+        [None, None, None]
+    ]
+
+
+
+if __name__ == '__main__':
+    main()
