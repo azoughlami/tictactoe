b4e3c81f45fcb1039b675e35a4ed34c4ec317e58
azoughlami <azoughlami@gmail.com>
2025-12-06 15:53:04 +0100

    find winner but with bug

--- game.py
+++ game.py
@@ -5,6 +5,10 @@
 # v ->   show the board
 # v -> choose location, mark it
 #   toggle active player
+from email.utils import UEMPTYSTRING
+
+from lib2to3.patcomp import tokenize_wrapper
+from tokenize import endpats
 
 
 def main():
@@ -33,12 +37,11 @@ def main():
             print("not an option, try again")
             continue
 
-        #toogle active player
+        # toogle active player
         active_player_index = (active_player_index + 1) % len(players)
 
     print(f"game over! {player} has won with the board: ")
     show_board(board)
-
 
 
 def choose_location(board, symbol):
@@ -77,8 +80,42 @@ def announce_turn(player):
 
 
 def find_winner(board):
-    # TODO: implement how we check for the winner
+    sequences = get_winning_sequences(board)
+
+    for cells in sequences:
+        symbol1 = cells[0]
+        if symbol1 and all(symbol1 == cell for cell in cells):
+            return True
+
     return False
+
+
+def get_winning_sequences(board):
+
+    sequences = []
+
+    # win by rows
+    rows = board
+    sequences.extend(rows)
+
+    # win by columns
+    columns = []
+    for col_idx in range(start := 0, stop := 3):  # stop value is exclusive
+        col = [
+            board[0][col_idx],
+            board[1][col_idx],
+            board[2][col_idx]
+        ]
+        sequences.append(col)
+
+    # win by diagonals
+    diagonals = [
+        [board[0][0], board[1][1], board[2][2]],
+        [board[0][2], board[1][1], board[2][0]]
+    ]
+    sequences.extend(diagonals)
+
+    return sequences
 
 
 if __name__ == '__main__':
